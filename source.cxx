6741551a1ce6ac2cc72b34ca33be098ba47b4a94
basman93 <basman93@users.noreply.github.com>
2017-07-17 03:22:25 +0200

    Added simple C++ file and cmake config.

---
+++ source.cxx
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "TestingConfig.h"
+
+int main(int argc, char* argv[])
+{
+  std::cout << "Version Number: " << Testing_VERSION_MAJOR << "." << Testing_VERSION_MINOR << std::endl;
+  std::cout << "Hello World!" << std::endl;
+  return 0;
+}
